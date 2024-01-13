7f7bc97da2dc993072c888c4f30f90e902e1add3
msi <meibrohimov.@gmail.com>
2024-01-13 13:44:43 +0500

    # django

--- apps/views.py
+++ apps/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def main(request):
-    return HttpResponse("Salom web saytga xush kelibsiz")
+    return render(request, "index.html")
